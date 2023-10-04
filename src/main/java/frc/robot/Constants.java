9bd34b706e703d43e8913c77893cff355d653544
mrhelland <tim.helland@lebanon.k12.or.us>
2023-10-04 16:52:45 -0700

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+package frc.robot;
+
+public class Constants {
+    public class Channels {
+        public final static int MotorA = 0;
+        public final static int MotorB = 0;
+        public final static int EncoderA = 0;
+        public final static int EncoderB = 0;
+    }
+
+    public class Parameters {
+
+    }
+}
